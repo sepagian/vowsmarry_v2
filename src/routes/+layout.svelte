aeb78ada756e4e3b489b5fce8d43fde37c4281b3
sepagian <rizkydana14@gmail.com>
2025-09-05 16:28:08 +0800

    style(layout): fix import quotes and add page title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
-	import "uno.css"
+	import 'uno.css';
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>vowsmarry</title>
 </svelte:head>
 
 {@render children?.()}
