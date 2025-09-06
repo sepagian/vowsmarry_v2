940a65830994351d0c985eb2e21e8199d83eb8a9
sepagian <rizkydana14@gmail.com>
2025-09-06 17:33:37 +0800

    feat(dashboard): add section cards to display dummy data

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,8 +1,35 @@
 <script>
+	import SectionCards from '$lib/components/sidebar/section-cards.svelte';
 	import SiteHeader from '$lib/components/sidebar/site-header.svelte';
+
+	const data = [
+		{
+			title: '12',
+			description: 'Tasks',
+			action: 'Active',
+			footer: 'Updated 5 minutes ago'
+		},
+		{
+			title: 'Rp50.000.000',
+			description: 'Budget',
+			footer: 'Updated 10 minutes ago'
+		},
+		{
+			title: '5',
+			description: 'Document',
+			action: 'Today',
+			footer: 'Updated 2 minutes ago'
+		},
+		{
+			title: '4',
+			description: 'Vendor',
+			action: 'This Month',
+			footer: 'Updated 1 hour ago'
+		}
+	];
 </script>
 
 <SiteHeader />
-<div class="flex p-6">
-	<h1>This is the dashboard</h1>
+<div class="flex flex-col">
+	<SectionCards {data} />
 </div>
