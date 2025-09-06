d22a02cd6fb858096be9c8839ad61d552dd1df1f
sepagian <rizkydana14@gmail.com>
2025-09-07 02:21:08 +0800

    feat(expense-table): add expense table with filtering, sorting, pagination, and actions

---
+++ src/lib/components/table/expense-table.svelte
@@ -0,0 +1,353 @@
+<script lang="ts">
+	import {
+		type ColumnDef,
+		type ColumnFiltersState,
+		type PaginationState,
+		type RowSelectionState,
+		type SortingState,
+		type VisibilityState,
+		getCoreRowModel,
+		getFilteredRowModel,
+		getPaginationRowModel,
+		getSortedRowModel
+	} from '@tanstack/table-core';
+	import { createRawSnippet } from 'svelte';
+	import ExpenseTableCheckbox from './expense-table-checkbox.svelte';
+	import ExpenseTableActions from './expense-table-actions.svelte';
+	import * as Table from '$lib/components/ui/table/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import {
+		FlexRender,
+		createSvelteTable,
+		renderComponent,
+		renderSnippet
+	} from '$lib/components/ui/data-table/index.js';
+
+	type Payment = {
+		id: string;
+		date: string;
+		category: string;
+		description: string;
+		amount: number;
+		status: 'Paid' | 'Pending';
+	};
+
+	const data: Payment[] = [
+		{
+			id: 'm5gr84i9',
+			amount: 2000000,
+			status: 'Paid',
+			date: '2023-10-01',
+			category: 'Decoration',
+			description: 'DP Tenda'
+		},
+		{
+			id: 'm5gr84i9',
+			amount: 1500000,
+			status: 'Paid',
+			date: '2023-10-01',
+			category: 'Catering',
+			description: 'DP Catering'
+		}
+	];
+
+	const columns: ColumnDef<Payment>[] = [
+		{
+			id: 'select',
+			header: ({ table }) =>
+				renderComponent(ExpenseTableCheckbox, {
+					checked: table.getIsAllPageRowsSelected(),
+					indeterminate: table.getIsSomePageRowsSelected() && !table.getIsAllPageRowsSelected(),
+					onCheckedChange: (value) => table.toggleAllPageRowsSelected(!!value),
+					'aria-label': 'Select all'
+				}),
+			cell: ({ row }) =>
+				renderComponent(ExpenseTableCheckbox, {
+					checked: row.getIsSelected(),
+					onCheckedChange: (value) => row.toggleSelected(!!value),
+					'aria-label': 'Select row'
+				}),
+			enableSorting: false,
+			enableHiding: false
+		},
+		{
+			accessorKey: 'date',
+			header: () => {
+				const amountHeaderSnippet = createRawSnippet(() => {
+					return {
+						render: () => `<div class="">Date</div>`
+					};
+				});
+				return renderSnippet(amountHeaderSnippet, '');
+			},
+			cell: ({ row }) => {
+				const emailSnippet = createRawSnippet<[string]>((getDate) => {
+					const date = getDate();
+					return {
+						render: () => `<div class="">${date}</div>`
+					};
+				});
+
+				return renderSnippet(emailSnippet, row.getValue('date'));
+			}
+		},
+		{
+			accessorKey: 'category',
+			header: () => {
+				const amountHeaderSnippet = createRawSnippet(() => {
+					return {
+						render: () => `<div class="">Category</div>`
+					};
+				});
+				return renderSnippet(amountHeaderSnippet, '');
+			},
+			cell: ({ row }) => {
+				const emailSnippet = createRawSnippet<[string]>((getCategory) => {
+					const category = getCategory();
+					return {
+						render: () => `<div class="text-left">${category}</div>`
+					};
+				});
+
+				return renderSnippet(emailSnippet, row.getValue('category'));
+			}
+		},
+		{
+			accessorKey: 'description',
+			header: () => {
+				const amountHeaderSnippet = createRawSnippet(() => {
+					return {
+						render: () => `<div class="">Description</div>`
+					};
+				});
+				return renderSnippet(amountHeaderSnippet, '');
+			},
+			cell: ({ row }) => {
+				const emailSnippet = createRawSnippet<[string]>((getDescription) => {
+					const description = getDescription();
+					return {
+						render: () => `<div class="">${description}</div>`
+					};
+				});
+
+				return renderSnippet(emailSnippet, row.getValue('description'));
+			}
+		},
+
+		{
+			accessorKey: 'amount',
+			header: () => {
+				const amountHeaderSnippet = createRawSnippet(() => {
+					return {
+						render: () => `<div class="text-right">Amount</div>`
+					};
+				});
+				return renderSnippet(amountHeaderSnippet, '');
+			},
+			cell: ({ row }) => {
+				const amountCellSnippet = createRawSnippet<[string]>((getAmount) => {
+					const amount = getAmount();
+					return {
+						render: () => `<div class="text-right font-medium">${amount}</div>`
+					};
+				});
+				const formatter = new Intl.NumberFormat('id-ID', {
+					style: 'currency',
+					currency: 'IDR'
+				});
+
+				return renderSnippet(
+					amountCellSnippet,
+					formatter.format(Number.parseFloat(row.getValue('amount')))
+				);
+			}
+		},
+		{
+			accessorKey: 'status',
+			header: 'Status',
+			cell: ({ row }) => {
+				const statusSnippet = createRawSnippet<[string]>((getStatus) => {
+					const status = getStatus();
+					return {
+						render: () => `<div class="capitalize">${status}</div>`
+					};
+				});
+				return renderSnippet(statusSnippet, row.getValue('status'));
+			}
+		},
+		{
+			id: 'actions',
+			enableHiding: false,
+			cell: ({ row }) => renderComponent(ExpenseTableActions, { id: row.original.id })
+		}
+	];
+
+	let pagination = $state<PaginationState>({ pageIndex: 0, pageSize: 10 });
+	let sorting = $state<SortingState>([]);
+	let columnFilters = $state<ColumnFiltersState>([]);
+	let rowSelection = $state<RowSelectionState>({});
+	let columnVisibility = $state<VisibilityState>({});
+
+	const table = createSvelteTable({
+		get data() {
+			return data;
+		},
+		columns,
+		state: {
+			get pagination() {
+				return pagination;
+			},
+			get sorting() {
+				return sorting;
+			},
+			get columnVisibility() {
+				return columnVisibility;
+			},
+			get rowSelection() {
+				return rowSelection;
+			},
+			get columnFilters() {
+				return columnFilters;
+			}
+		},
+		getCoreRowModel: getCoreRowModel(),
+		getPaginationRowModel: getPaginationRowModel(),
+		getSortedRowModel: getSortedRowModel(),
+		getFilteredRowModel: getFilteredRowModel(),
+		onPaginationChange: (updater) => {
+			if (typeof updater === 'function') {
+				pagination = updater(pagination);
+			} else {
+				pagination = updater;
+			}
+		},
+		onSortingChange: (updater) => {
+			if (typeof updater === 'function') {
+				sorting = updater(sorting);
+			} else {
+				sorting = updater;
+			}
+		},
+		onColumnFiltersChange: (updater) => {
+			if (typeof updater === 'function') {
+				columnFilters = updater(columnFilters);
+			} else {
+				columnFilters = updater;
+			}
+		},
+		onColumnVisibilityChange: (updater) => {
+			if (typeof updater === 'function') {
+				columnVisibility = updater(columnVisibility);
+			} else {
+				columnVisibility = updater;
+			}
+		},
+		onRowSelectionChange: (updater) => {
+			if (typeof updater === 'function') {
+				rowSelection = updater(rowSelection);
+			} else {
+				rowSelection = updater;
+			}
+		}
+	});
+</script>
+
+<div class="w-full">
+	<div class="flex items-center py-4 gap-4">
+		<Input
+			placeholder="Filter category..."
+			value={(table.getColumn('category')?.getFilterValue() as string) ?? ''}
+			oninput={(e) => table.getColumn('category')?.setFilterValue(e.currentTarget.value)}
+			onchange={(e) => {
+				table.getColumn('category')?.setFilterValue(e.currentTarget.value);
+			}}
+			class="max-w-sm border-1 border-neutral-200"
+		/>
+		<DropdownMenu.Root>
+			<DropdownMenu.Trigger>
+				{#snippet child({ props })}
+					<Button {...props} variant="outline" size="sm" class="ml-auto items-center">
+						<div class="i-lucide:columns-2"></div>
+						View
+						<div class="i-lucide:chevron-down ml-2"></div>
+					</Button>
+				{/snippet}
+			</DropdownMenu.Trigger>
+			<DropdownMenu.Content align="end">
+				{#each table.getAllColumns().filter((col) => col.getCanHide()) as column (column)}
+					<DropdownMenu.CheckboxItem
+						class="capitalize"
+						bind:checked={() => column.getIsVisible(), (v) => column.toggleVisibility(!!v)}
+					>
+						{column.id}
+					</DropdownMenu.CheckboxItem>
+				{/each}
+			</DropdownMenu.Content>
+		</DropdownMenu.Root>
+		<Button variant="outline" size="sm" class="items-center">
+			<div class="i-lucide:plus"></div>
+			<span class="hidden lg:inline">Add Expense</span>
+		</Button>
+	</div>
+	<div class="rounded-md border">
+		<Table.Root>
+			<Table.Header>
+				{#each table.getHeaderGroups() as headerGroup (headerGroup.id)}
+					<Table.Row>
+						{#each headerGroup.headers as header (header.id)}
+							<Table.Head class="[&:has([role=checkbox])]:pl-3">
+								{#if !header.isPlaceholder}
+									<FlexRender
+										content={header.column.columnDef.header}
+										context={header.getContext()}
+									/>
+								{/if}
+							</Table.Head>
+						{/each}
+					</Table.Row>
+				{/each}
+			</Table.Header>
+			<Table.Body>
+				{#each table.getRowModel().rows as row (row.id)}
+					<Table.Row data-state={row.getIsSelected() && 'selected'}>
+						{#each row.getVisibleCells() as cell (cell.id)}
+							<Table.Cell class="[&:has([role=checkbox])]:pl-3">
+								<FlexRender content={cell.column.columnDef.cell} context={cell.getContext()} />
+							</Table.Cell>
+						{/each}
+					</Table.Row>
+				{:else}
+					<Table.Row>
+						<Table.Cell colspan={columns.length} class="h-24 text-center">No results.</Table.Cell>
+					</Table.Row>
+				{/each}
+			</Table.Body>
+		</Table.Root>
+	</div>
+	<div class="flex items-center justify-end space-x-2 pt-4">
+		<div class="text-muted-foreground flex-1 text-sm">
+			{table.getFilteredSelectedRowModel().rows.length} of
+			{table.getFilteredRowModel().rows.length} row(s) selected.
+		</div>
+		<div class="space-x-2">
+			<Button
+				variant="outline"
+				size="sm"
+				onclick={() => table.previousPage()}
+				disabled={!table.getCanPreviousPage()}
+			>
+				Previous
+			</Button>
+			<Button
+				variant="outline"
+				size="sm"
+				onclick={() => table.nextPage()}
+				disabled={!table.getCanNextPage()}
+			>
+				Next
+			</Button>
+		</div>
+	</div>
+</div>
