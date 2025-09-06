9254337df84eb76171bed7e53fc85003947ba9ac
sepagian <rizkydana14@gmail.com>
2025-09-07 02:21:43 +0800

    refactor(section): update todo section content and layout

--- src/lib/components/section/section-todo.svelte
+++ src/lib/components/section/section-todo.svelte
@@ -4,20 +4,20 @@
 </script>
 
 <div class="flex flex-col px-6">
-	<h2 class="text-base font-bold text-neutral-600">To do</h2>
+	<h2 class="text-base font-bold text-neutral-600">Tasks</h2>
 </div>
 
 <div class="flex flex-col px-6">
 	<Card.Root class="@container/card flex flex-row justify-between items-center">
-		<div class="sm:w-full">
+		<div class="w-full">
 			<Card.Header>
-				<Card.Title>Complete your wedding information</Card.Title>
+				<Card.Title class="leading-6 text-base">Almost ready to plan your big day 🎉</Card.Title>
 			</Card.Header>
-			<Card.Content>
-				<p>Submit your wedding information to start planning</p>
+			<Card.Content class="text-sm">
+				<p>Complete your wedding profile to see tasks, invitations, and more.</p>
 			</Card.Content>
 		</div>
-		<div class="justify-end flex px-6">
+		<div class="w-fit justify-end flex px-6">
 			<Button variant="outline" size="icon"><div class="i-lucide:chevron-right"></div></Button>
 		</div>
 	</Card.Root>
