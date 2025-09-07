13461413f0695057280ae65a93cf56e5876e176d
sepagian <rizkydana14@gmail.com>
2025-09-07 19:51:28 +0800

    style(section): improve layout and spacing

--- src/lib/components/section/section-todo.svelte
+++ src/lib/components/section/section-todo.svelte
@@ -43,84 +43,86 @@
 	}
 </script>
 
-<!-- Header -->
-<div class="flex px-6 justify-between items-center">
-	<h2 class="text-base font-bold text-neutral-600">Tasks</h2>
-	<div class="flex flex-1 items-center justify-end gap-4">
-		<Select.Root type="single" name="taskFilter" bind:value={filter}>
-			<Select.Trigger class="w-40 border border-neutral-300">
-				{filterOptions.find((f) => f.value === filter)?.label}
-			</Select.Trigger>
-			<Select.Content>
-				<Select.Group>
-					{#each filterOptions as f (f.value)}
-						<Select.Item value={f.value} label={f.label}>
-							{f.label}
-						</Select.Item>
-					{/each}
-				</Select.Group>
-			</Select.Content>
-		</Select.Root>
+<div class="flex flex-col gap-2">
+	<!-- Header -->
+	<div class="flex px-6 justify-between items-center">
+		<h2 class="text-base font-bold text-neutral-600">Tasks</h2>
+		<div class="flex flex-1 items-center justify-end gap-4">
+			<Select.Root type="single" name="taskFilter" bind:value={filter}>
+				<Select.Trigger class="w-40 border border-neutral-300">
+					{filterOptions.find((f) => f.value === filter)?.label}
+				</Select.Trigger>
+				<Select.Content>
+					<Select.Group>
+						{#each filterOptions as f (f.value)}
+							<Select.Item value={f.value} label={f.label}>
+								{f.label}
+							</Select.Item>
+						{/each}
+					</Select.Group>
+				</Select.Content>
+			</Select.Root>
 
-		<Dialog.Root>
-			<Dialog.Trigger class={buttonVariants({ variant: 'outline', size: 'default' })}>
-				<div class="i-lucide:plus p-2"></div>
-				<span class="hidden lg:inline">Add New Task</span>
-			</Dialog.Trigger>
-			<DialogTask />
-		</Dialog.Root>
+			<Dialog.Root>
+				<Dialog.Trigger class={buttonVariants({ variant: 'outline', size: 'default' })}>
+					<div class="i-lucide:plus p-2"></div>
+					<span class="hidden lg:inline">Add New Task</span>
+				</Dialog.Trigger>
+				<DialogTask />
+			</Dialog.Root>
+		</div>
 	</div>
-</div>
 
-<!-- Profile completion alert -->
-<div class="flex flex-col px-6 gap-4">
-	<Card.Root class="@container/card flex flex-row justify-between items-center">
-		<div class="w-full">
-			<Card.Header>
-				<Card.Title class="leading-6 text-base">Almost ready to plan your big day 🎉</Card.Title>
-			</Card.Header>
-			<Card.Content class="text-sm">
-				<p>Complete your wedding profile to see tasks, invitations, and more.</p>
-			</Card.Content>
-		</div>
-		<div class="w-fit justify-end flex px-6">
-			<Button variant="outline" size="icon">
-				<div class="i-lucide:chevron-right"></div>
-			</Button>
-		</div>
-	</Card.Root>
+	<!-- Profile completion alert -->
+	<div class="flex flex-col px-6 gap-4">
+		<Card.Root class="@container/card flex flex-row justify-between items-center">
+			<div class="w-full">
+				<Card.Header>
+					<Card.Title class="leading-6 text-base">Almost ready to plan your big day 🎉</Card.Title>
+				</Card.Header>
+				<Card.Content class="text-sm">
+					<p>Complete your wedding profile to see tasks, invitations, and more.</p>
+				</Card.Content>
+			</div>
+			<div class="w-fit justify-end flex px-6">
+				<Button variant="outline" size="icon">
+					<div class="i-lucide:chevron-right"></div>
+				</Button>
+			</div>
+		</Card.Root>
 
-	<!-- Task list -->
-	{#if filteredTasks.length > 0}
-		{#each filteredTasks as task (task.title)}
-			<Card.Root class="@container/card flex flex-row justify-between items-center">
-				<div class="pl-6 flex items-center w-full">
-					<input
-						type="checkbox"
-						checked={task.done}
-						onchange={() => toggleTask(task)}
-						class="mt-1 w-4 h-4 rounded border-neutral-300 text-primary focus:ring-primary"
-					/>
-					<div>
-						<Card.Header>
-							<Card.Title
-								class={`leading-6 text-base ${task.done ? 'line-through text-neutral-400' : ''}`}
-							>
-								{task.title}
-							</Card.Title>
-						</Card.Header>
-						<Card.Content class="text-sm">
-							<p class={task.done ? 'line-through text-neutral-400' : ''}>
-								{task.description}
-							</p>
-						</Card.Content>
+		<!-- Task list -->
+		{#if filteredTasks.length > 0}
+			{#each filteredTasks as task (task.title)}
+				<Card.Root class="@container/card flex flex-row justify-between items-center">
+					<div class="pl-6 flex items-center w-full">
+						<input
+							type="checkbox"
+							checked={task.done}
+							onchange={() => toggleTask(task)}
+							class="mt-1 w-4 h-4 rounded border-neutral-300 text-primary focus:ring-primary"
+						/>
+						<div>
+							<Card.Header>
+								<Card.Title
+									class={`leading-6 text-base ${task.done ? 'line-through text-neutral-400' : ''}`}
+								>
+									{task.title}
+								</Card.Title>
+							</Card.Header>
+							<Card.Content class="text-sm">
+								<p class={task.done ? 'line-through text-neutral-400' : ''}>
+									{task.description}
+								</p>
+							</Card.Content>
+						</div>
 					</div>
-				</div>
-			</Card.Root>
-		{/each}
-	{:else}
-		<Label class="flex items-center justify-center h-12 gap-3 rounded-lg border p-3"
-			>You have not completed any task yet</Label
-		>
-	{/if}
+				</Card.Root>
+			{/each}
+		{:else}
+			<Label class="flex items-center justify-center h-12 gap-3 rounded-lg border p-3"
+				>You have not completed any task yet</Label
+			>
+		{/if}
+	</div>
 </div>
