bc7ff0a2b49001f9fd34defca4ca53581e305bd5
sepagian <rizkydana14@gmail.com>
2025-09-07 15:16:37 +0800

    refactor(sidebar): generate breadcrumbs dynamically from URL path

--- src/lib/components/sidebar/site-header.svelte
+++ src/lib/components/sidebar/site-header.svelte
@@ -1,47 +1,66 @@
 <script lang="ts">
 	import * as Breadcrumb from '$lib/components/ui/breadcrumb/index';
 	import * as Sidebar from '$lib/components/ui/sidebar/index';
-	import { Separator } from '$lib/components/ui/separator/index';
-	import AppearanceSwitcher from '../appearance-switcher.svelte';
-	import { Button } from '$lib/components/ui/button/index';
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index';
+	import { Separator } from '$lib/components/ui/separator/index';
+	import { Button } from '$lib/components/ui/button/index';
+	import AppearanceSwitcher from '../appearance-switcher.svelte';
+	import { page } from '$app/state';
+
+	const breadcrumbs = $derived(
+		page.url.pathname
+			.split('/')
+			.filter(Boolean) // Remove empty segments from leading/trailing slashes
+			.map((segment, index, arr) => {
+				const href = '/' + arr.slice(0, index + 1).join('/');
+				// Capitalize the segment for display
+				const title = segment.replace(/-/g, ' ').replace(/\b\w/g, (c) => c.toUpperCase());
+				return { title, href, isLast: index === arr.length - 1 };
+			})
+	);
 </script>
 
-<div
-	class="group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 flex h-16 p-6 justify-between items-center gap-2 transition-[width,height] ease-linear"
+<header
+	class="sticky top-0 z-50 flex items-center border-b group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear"
 >
-	<div class="flex items-center gap-2">
-		<Sidebar.Trigger class="h-4 w-4" />
-		<Separator orientation="vertical" class="mr-2 data-[orientation=vertical]:h-4" />
-		<Breadcrumb.Root>
-			<Breadcrumb.List>
-				<Breadcrumb.Item class="hidden md:block">
-					<Breadcrumb.Link href="#">Building Your Application</Breadcrumb.Link>
-				</Breadcrumb.Item>
-				<Breadcrumb.Separator class="hidden md:block" />
-				<Breadcrumb.Item>
-					<Breadcrumb.Page>Data Fetching</Breadcrumb.Page>
-				</Breadcrumb.Item>
-			</Breadcrumb.List>
-		</Breadcrumb.Root>
+	<div class="h-14 flex w-full items-center gap-2 justify-between px-6">
+		<div class="flex items-center gap-2">
+			<Sidebar.Trigger class="h-4 w-4" />
+			<Separator orientation="vertical" class="mr-2 data-[orientation=vertical]:h-4" />
+			<Breadcrumb.Root class="hidden sm:block">
+				<Breadcrumb.List>
+					{#each breadcrumbs as crumb, i (i)}
+						<Breadcrumb.Item>
+							{#if crumb.isLast}
+								<Breadcrumb.Page>{crumb.title}</Breadcrumb.Page>
+							{:else}
+								<Breadcrumb.Link href={crumb.href}>{crumb.title}</Breadcrumb.Link>
+							{/if}
+						</Breadcrumb.Item>
+						{#if !crumb.isLast}
+							<Breadcrumb.Separator />
+						{/if}
+					{/each}
+				</Breadcrumb.List>
+			</Breadcrumb.Root>
+		</div>
+		<div class="flex justify-end gap-4">
+			<DropdownMenu.Root>
+				<DropdownMenu.Trigger>
+					<Button variant="outline" size="sm" class="h-9"
+						>Quick Actions
+						<div class="i-lucide:chevron-down ml-auto"></div></Button
+					>
+				</DropdownMenu.Trigger>
+				<DropdownMenu.Content>
+					<DropdownMenu.Group>
+						<DropdownMenu.Item>New Task</DropdownMenu.Item>
+						<DropdownMenu.Item>Add Expense</DropdownMenu.Item>
+						<DropdownMenu.Item>Upload Document</DropdownMenu.Item>
+					</DropdownMenu.Group>
+				</DropdownMenu.Content>
+			</DropdownMenu.Root>
+			<AppearanceSwitcher />
+		</div>
 	</div>
-	<div class="flex justify-end gap-4">
-		<DropdownMenu.Root>
-			<DropdownMenu.Trigger>
-				<Button variant="outline" size="sm" class="h-9"
-					>Quick Actions
-					<div class="i-lucide:chevron-down ml-auto"></div></Button
-				>
-			</DropdownMenu.Trigger>
-			<DropdownMenu.Content>
-				<DropdownMenu.Group>
-					<DropdownMenu.Item>New Task</DropdownMenu.Item>
-					<DropdownMenu.Item>Add Expense</DropdownMenu.Item>
-					<DropdownMenu.Item>Upload Document</DropdownMenu.Item>
-				</DropdownMenu.Group>
-			</DropdownMenu.Content>
-		</DropdownMenu.Root>
-
-		<AppearanceSwitcher />
-	</div>
-</div>
+</header>
