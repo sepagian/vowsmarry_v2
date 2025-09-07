cde9e31bf976d1312a02cf385c1970a2c6a14b79
sepagian <rizkydana14@gmail.com>
2025-09-07 15:16:06 +0800

    feat(dashboard): update budget data and add task list

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-	import SiteHeader from '$lib/components/sidebar/site-header.svelte';
 	import SectionCards from '$lib/components/section/section-cards.svelte';
 	import SectionTodo from '$lib/components/section/section-todo.svelte';
 	import SectionBudget from '$lib/components/section/section-budget.svelte';
@@ -32,26 +31,43 @@
 
 	const budgetData = [
 		{
-			title: 'Rp20.000.000',
+			title: 'Rp50.000.000',
 			description: 'Total Budget',
 			footer: 'Updated 1 day ago'
 		},
 		{
-			title: 'Rp15.000.000',
+			title: 'Rp30.000.000',
 			description: 'Amount Spent',
 			footer: 'Updated 2 days ago'
 		},
 		{
-			title: 'Rp5.000.000',
+			title: 'Rp20.000.000',
 			description: 'Remaining Budget',
 			footer: 'Updated 3 days ago'
 		}
 	];
+
+	const tasks = [
+		{
+			title: 'Create Wedding Website',
+			description: 'Design and launch the wedding website for guests.',
+			status: 'on-going'
+		},
+		{
+			title: 'Send Invitations',
+			description: 'Mail out physical invitations to all guests.',
+			status: 'pending'
+		},
+		{
+			title: 'Book Photographer',
+			description: 'Finalize and book the wedding photographer.',
+			status: 'complete'
+		}
+	];
 </script>
 
-<SiteHeader />
-<div class="flex flex-col gap-4">
+<div class="flex flex-1 flex-col gap-4 py-4 min-h-[calc(100vh-3rem)]">
 	<SectionCards {data} />
-	<SectionTodo />
+	<SectionTodo {tasks} />
 	<SectionBudget {budgetData} />
 </div>
