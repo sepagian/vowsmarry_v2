c74fd647d2874ea4a7efd3a2226ac63ec5d2bb83
sepagian <rizkydana14@gmail.com>
2025-09-08 20:33:59 +0800

    refactor(section): improve layout and card header structure

--- src/lib/components/section/section-cards.svelte
+++ src/lib/components/section/section-cards.svelte
@@ -22,20 +22,22 @@
 	</div>
 
 	<div
-		class="flex gap-4 overflow-x-auto scrollbar-none scroll-smooth sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-6"
+		class="flex gap-4 overflow-x-auto scrollbar-none scroll-smooth sm:grid md:grid lg:grid-cols-3 xl:grid-cols-4 px-6"
 	>
 		{#each overviewCards as data (data.description)}
 			<Card.Root class="@container/card shrink-0 w-64 sm:w-auto gap-2">
-				<Card.Header>
-					<Card.Description>{data.description}</Card.Description>
+				<Card.Header class="flex flex-col gap-2">
+					<div class="flex flex-row justify-between items-center w-full">
+						<Card.Description>{data.description}</Card.Description>
+						<Card.Action>
+							{#if data.action}
+								<Badge variant="outline">{data.action}</Badge>
+							{/if}
+						</Card.Action>
+					</div>
 					<Card.Title class="@[250px]/card:text-3xl text-2xl font-semibold tabular-nums">
 						{data.title}
 					</Card.Title>
-					<Card.Action>
-						{#if data.action}
-							<Badge variant="outline">{data.action}</Badge>
-						{/if}
-					</Card.Action>
 				</Card.Header>
 				<Card.Footer class="flex-col items-start text-sm">
 					<div class="line-clamp-1 flex font-medium">
