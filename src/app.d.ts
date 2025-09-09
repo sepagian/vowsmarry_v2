41a0a24231fbf22fa1e3b87e5db47d8ee92a0aea
sepagian <rizkydana14@gmail.com>
2025-09-09 23:38:17 +0800

    feat(types): add Category and Task types to global declarations

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,6 +1,25 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
+	type Category =
+		| 'accommodation'
+		| 'catering'
+		| 'decoration'
+		| 'entertainment'
+		| 'makeup-attire'
+		| 'paperwork'
+		| 'photo-video'
+		| 'venue'
+		| 'miscellaneous';
+
+	type Task = {
+		id: string;
+		title: string;
+		description?: string;
+		category: (typeof Category)[number];
+		priority?: 'Low' | 'Medium' | 'High';
+		status: 'Pending' | 'On Progress' | 'Completed';
+	};
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
