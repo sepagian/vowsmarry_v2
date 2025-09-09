ed978bdbdcd20e8488b92cf4e6960d9f495f380b
sepagian <rizkydana14@gmail.com>
2025-09-09 23:38:04 +0800

    style(task-table-actions): add icons and improve status button UI

--- src/lib/components/table/task-table-actions.svelte
+++ src/lib/components/table/task-table-actions.svelte
@@ -1,25 +1,35 @@
 <script lang="ts">
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
 
-	export let status: string;
-	export let onChange: (newStatus: string) => void;
+	export let status: Task['status'];
+	export let onChange: (newStatus: Task['status']) => void;
 
 	const statusOptions = [
-		{ value: 'Complete', color: 'bg-green-600 text-white' },
-		{ value: 'Pending', color: 'bg-yellow-100 text-yellow-800' },
-		{ value: 'On Progress', color: 'bg-sky-200 text-sky-800' }
+		{ value: 'Complete', color: 'bg-green-600 text-white', icon: 'i-lucide:alarm-clock-check' },
+		{
+			value: 'Pending',
+			color: 'bg-yellow-100 text-yellow-800',
+			icon: 'i-lucide:alarm-clock-minus'
+		},
+		{ value: 'On Progress', color: 'bg-sky-200 text-sky-800', icon: 'i-lucide:alarm-clock' }
 	];
 
-	function getColor(status: string) {
+	function getColor(status: Task['status']) {
 		return (
 			statusOptions.find((s) => s.value === status)?.color ?? 'bg-neutral-100 text-neutral-800'
 		);
+	}
+	function getIcon(status: Task['status']) {
+		return statusOptions.find((s) => s.value === status)?.icon ?? 'i-lucide:alarm-clock-minus';
 	}
 </script>
 
 <DropdownMenu.Root>
 	<DropdownMenu.Trigger class={`rounded-md text-sm ${getColor(status)}`}>
-		<button class={`rounded-md px-3 py-1 text-sm font-medium ${getColor(status)}`}>
+		<button
+			class={`rounded-md px-3 inline-flex items-center gap-2 py-1 text-sm font-medium ${getColor(status)}`}
+		>
+			<div class={`${getIcon(status)}`}></div>
 			{status}
 		</button>
 	</DropdownMenu.Trigger>
@@ -33,7 +43,10 @@
 					<span class="h-4 w-4"></span>
 				{/if}
 
-				<span class={`inline-flex rounded-md px-2 py-1 text-xs font-medium ${s.color}`}>
+				<span
+					class={`inline-flex rounded-md items-center gap-2 px-2 py-1 text-xs font-medium ${s.color}`}
+				>
+					<div class={`${s.icon}`}></div>
 					{s.value}
 				</span>
 			</DropdownMenu.Item>
